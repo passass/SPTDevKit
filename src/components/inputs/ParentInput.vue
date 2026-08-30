75a152336ff14e56a933eb58530e5a4ba458a1c0
asd <asd>
2026-08-30 17:53:57 +1000

    forgot

---
+++ src/components/inputs/ParentInput.vue
@@ -0,0 +1,32 @@
+<template>
+	<AdvancedSelectInput
+	    v-model="props.modelValue"
+		:itemsOverride="items"
+	/>
+</template>
+
+<script setup lang="ts">
+import AdvancedSelectInput from './AdvancedSelectInput.vue';
+import { computed } from 'vue';
+import { Field, getIdFieldValue, RecordSchema } from '@/types/fields/fields';
+const props = defineProps<{
+	modelValue: string | null;
+	recordSchema: RecordSchema;
+}>();
+
+const items = computed(() => {
+	const result: Map<string, object> = new Map()
+
+	if (Array.isArray(props.recordSchema.parent)) {
+		for (const el of props.recordSchema.parent) {
+			const id = getIdFieldValue(el)
+			if ("_tpl" in el && id) {
+				result.set(id, {"id": el["_tpl"]})
+			}
+		}
+	}
+
+	return result
+})
+
+</script>
